422eac3855fc95323cc74fa508c98e8ce86fa386
alishasonawalla <asonawalla@nyu.edu>
2019-02-24 14:43:36 -0500

    Initial file commit

---
+++ src/main/java/org/smartworldapps/voyager/TravelApplication.java
@@ -0,0 +1,12 @@
+package org.smartworldapps.voyager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelApplication {
+
+    public static void main(String [] args) {
+        SpringApplication.run(TravelApplication.class, args);
+    }
+}
